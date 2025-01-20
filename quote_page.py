3df05b505743ceb132bf2b12cf6d3ce87f9729e0
Adhithan <131857438+Adhithan21@users.noreply.github.com>
2025-01-20 18:14:13 +0530

    quote_page.py

--- quote_page.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Get the quote from the URL query parameters
-quote = st.experimental_get_query_params().get("quote", ["No quote provided"])[0]
-
-# Display headline and the quote
-st.title("Positive Thought")
-st.write(f"### {quote}")
