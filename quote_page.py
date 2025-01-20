7026b93c51f4de4c300c701432dba0543d0cb57e
Adhithan <131857438+Adhithan21@users.noreply.github.com>
2025-01-20 14:11:17 +0530

    quote_page.py

--- quote_page.py
+++ quote_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-# Main Streamlit app to display the quote from URL parameters
+# Get the quote from the URL query parameters
 quote = st.experimental_get_query_params().get("quote", ["No quote provided"])[0]
 
 # Display headline and the quote
